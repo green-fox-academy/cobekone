2f01b0b20c48c5d131a191ae79e5a2f366a1e86b
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-10-23 11:05:41 +0200

    seconds in a day added

---
+++ week-03/day-02/seconds in a day/index.js
@@ -0,0 +1,4 @@
+var currentHours = 23;
+var currentMinutes = 58;
+var currentSeconds = 1;
+console.log((24 * 60 * 60) - ((currentHours * 60 * 60) + (currentMinutes * 60) + currentSeconds));
---
+++ week-03/day-02/seconds in a day/index.ts
@@ -0,0 +1,5 @@
+let currentHours: number = 23;
+let currentMinutes: number = 58;
+let currentSeconds: number = 1;
+
+console.log ((24 * 60 * 60) - ((currentHours * 60 * 60) + (currentMinutes * 60) + currentSeconds));
