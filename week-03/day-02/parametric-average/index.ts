05ab546f17c4674a22090286a091d5fb44d7ae5f
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-10-23 14:31:23 +0200

    parametric-average added

---
+++ week-03/day-02/parametric-average/index.ts
@@ -0,0 +1,11 @@
+'use strict';
+let number: number = 2;
+let avg: number = 0;
+let sum: number = 0;
+
+  for (let a = 1; a <= number; a++) {
+      sum = sum + a
+           
+  }
+  console.log ('Sum: ' + sum)
+  console.log ('Average: ' + sum / number)
