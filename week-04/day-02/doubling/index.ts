8478c960e6ab2a2e8bacba684d869977f5ee5984
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-10-30 16:17:43 +0200

    doubling added

---
+++ week-04/day-02/doubling/index.ts
@@ -0,0 +1,14 @@
+// -  Create a variable named `baseNumber` and assign the value `123` to it
+// -  Create a function called `doubleNumber()` that doubles it's input parameter
+//    and returns the doubled value
+// -  Log the result of `doubleNumber(baseNumber)` to the console
+
+'use strict'
+
+let basenumber: number = 123;
+
+function doubleNumber() {
+    console.log (basenumber * 2);
+}
+
+doubleNumber()
