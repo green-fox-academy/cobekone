a398d1296c40087908feab0825dd28787e62b762
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-10-30 16:24:05 +0200

    greet task added

---
+++ week-04/day-02/greet/index.ts
@@ -0,0 +1,12 @@
+// -  Create variable named `nameToGreet` and assign the value `Green Fox` to it
+// -  Create a function called `greet()` that greets its input parameter
+//     - It prints a greeting message e.g. `Greetings dear Green Fox`
+// -  Greet `nameToGreet`
+
+let nameToGreet: string = "Green Fox";
+
+function greet(nameToGreet: string) {
+    console.log ("Greetings dear " + nameToGreet)
+}
+
+greet("srác")
