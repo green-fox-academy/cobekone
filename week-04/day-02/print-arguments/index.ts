27b20562df590d3bd025b89498106943d62a967f
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-10-30 18:24:27 +0200

    unique task added

---
+++ week-04/day-02/print-arguments/index.ts
@@ -0,0 +1,11 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+'use strict'
+
+function printParams(values: any[]) {
+    console.log (values);
+}
+
+printParams(["az", "meg", "ez", 8])
---
+++ week-04/day-02/unique/index.ts
@@ -0,0 +1,19 @@
+//  Create a function that takes a list of numbers as a parameter
+//  and returns a list of numbers where every number is unique (occurs only once)
+
+function findUniqueItems(arr: number[]) {
+console.log (arr);
+let b = arr.length;
+for (let a = 0; a < arr.length; a++) {    
+for (let b = arr.length; b > a; b--) {
+    if (arr[a] === arr[b]) {
+        console.log (arr[a]);
+        arr.splice (b, 1);
+    }
+}}
+console.log (arr)
+}
+
+//  Example
+console.log(findUniqueItems([1, 11, 34, 11, 52, 11, 11, 11, 61, 1, 34]))
+//  should print: `[1, 11, 34, 52, 61]`
