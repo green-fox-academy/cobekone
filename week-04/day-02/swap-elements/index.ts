202525a9f290bfe440617bd8abf7c3ef0362daf0
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-10-30 13:36:09 +0200

    swap-elements added

---
+++ week-04/day-02/swap-elements/index.ts
@@ -0,0 +1,10 @@
+// -  Create a variable named `names` with the following content:
+//   `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `names`
+'use strict'
+
+let names: string[] = ["Arthur", "Boe", "Chloe"];
+
+[names[2], names[0]] = [names [0], names[2]];
+
+console.log(names)
