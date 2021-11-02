e91236d05370e4dc7a5333f2f6f1e0e5b0a48a1f
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-11-02 20:22:31 +0100

    subint task added

---
+++ week-04/day-02/subint/index.ts
@@ -0,0 +1,19 @@
+'use strict';
+//  Create a function that takes a number and an array of numbers as a parameter
+//  and returns the indices of the numbers of the array which contain the given number
+//  or returns an empty list (if the number is not part of any of the numbers in the array)
+
+function findMatchingIndexes(toFind:number, inArray:number[]) {
+    let result: any[] = [];
+    result = inArray.filter ((value) => {
+    return `${value}`.search(`${toFind}`) >= 0;
+    })
+    return result;
+}
+
+// Example
+console.log(findMatchingIndexes(1, [1, 11, 34, 52, 61]));
+// should print: `[0, 1, 4]`
+console.log(findMatchingIndexes(9, [1, 11, 34, 52, 61]));
+// should print: '[]'
+
