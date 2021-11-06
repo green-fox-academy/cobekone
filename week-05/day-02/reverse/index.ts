dd50ba629871926b3d32c146698ccb20f0868d7d
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-11-06 14:23:46 +0100

    reverse task added

---
+++ week-05/day-02/reverse/index.ts
@@ -0,0 +1,22 @@
+
+'use strict';
+// Create a function that can reverse a string, which is passed as the parameter
+// Use it on this reversed string to check it!
+
+let toBeReversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+function reverse(value: string) {
+    let splitted: string[] = toBeReversed.split("");
+    let reversed: string[] = [];
+
+    for (let i = 0; i < toBeReversed.length; i++) {
+        reversed[i] = splitted [splitted.length - 1 - i]
+
+    }
+    let reversedJoined: string = (reversed.join(''));
+    
+    return (reversedJoined);
+}
+
+console.log(reverse(toBeReversed));
+
+export = reverse;
