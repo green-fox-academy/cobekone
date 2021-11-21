e14f07ad8cff3c0c58af9abf6aa86c23dcc75a16
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-11-21 23:25:24 +0100

    Super Hexagon task got some extra sliders to config the settings on the go

--- week-07/day-03/super-hexagon/super-hexagon.ts
+++ week-07/day-03/super-hexagon/super-hexagon.ts
@@ -6,8 +6,25 @@ export {};
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
-//Egyenlő oldalú hatszög rajzoló: t = oldal hossza; o = középpont koordinátái
+let kisHexagonOldalhossz: number = 10;
+let nagyHexagonOldalszam: number = 2;
 
+var scale = document.getElementById("scaleID");
+scale.oninput = function() {
+    kisHexagonOldalhossz = parseInt (scale.value);
+    console.log ('Scale csuszkaval beallitva: ' + kisHexagonOldalhossz)
+    drawIt()
+  }
+
+  var sideCount = document.getElementById("sideCountID");
+sideCount.oninput = function() {
+    nagyHexagonOldalszam = parseInt (sideCount.value);
+    console.log ('Sidecount csuszkaval beallitva: ' + kisHexagonOldalhossz)
+    drawIt()
+  }
+
+
+//Egyenlő oldalú hatszög rajzoló function: t = oldal hossza; o = középpont koordinátái
 function drawHexagon(t: number, ox: number, oy: number) {
     ctx.beginPath();
     //középpont ideiglenes jelölése:
@@ -30,22 +47,27 @@ function drawHexagon(t: number, ox: number, oy: number) {
     ctx.stroke();
 }
 
-let kisHexagonOldalhossz: number = 7;
-let nagyHexagonOldalszam: number = 17;
-let r: number = (Math.sqrt(3) / 2) * kisHexagonOldalhossz
-let nagyHexagonMagassaga: number = 2 * nagyHexagonOldalszam - 1
-let utolsoXKoordinata: number = 0;
+
+
+
+function drawIt() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    let r: number = (Math.sqrt(3) / 2) * kisHexagonOldalhossz
+    let nagyHexagonMagassaga: number = 2 * nagyHexagonOldalszam - 1
+    let utolsoXKoordinata: number = 0;
 
 //Növekvő rész:
 
 for (let i = 0; i < nagyHexagonOldalszam; i++) {
     for (let j = 0; j < nagyHexagonOldalszam + i; j++) {
         
-        drawHexagon (kisHexagonOldalhossz, 
+            drawHexagon (kisHexagonOldalhossz, 
 
-            kisHexagonOldalhossz + (kisHexagonOldalhossz * 1.5 * i), 
+            kisHexagonOldalhossz * 1.5 * i + kisHexagonOldalhossz, 
 
-            (Math.sqrt(3) * kisHexagonOldalhossz * j) + nagyHexagonOldalszam * r - r * i)
+            (Math.sqrt(3) * kisHexagonOldalhossz * j) + nagyHexagonOldalszam * r - r * i);
+
             utolsoXKoordinata = kisHexagonOldalhossz + (kisHexagonOldalhossz * 1.5 * i)
     }
 }
@@ -62,7 +84,7 @@ for (let i = 1; i < nagyHexagonOldalszam; i++) {
             (r * j * 2) + i * r + r)
     }
 }
+}
 
 
-
-
+drawIt()
