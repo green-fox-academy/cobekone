9ea301c9d7be68f5d429b7f4f3e73487350c98c2
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-11-25 19:58:26 +0100

    envelope-star-extra added for some cosinus sinus fun

--- week-07/day-03/envelope-star-extra/envelope-star-ts.ts
+++ week-07/day-03/envelope-star-extra/envelope-star-ts.ts
@@ -4,18 +4,58 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 export {};
 
+
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
-//ideiglenes koordináta jelölő function
+let h: number = 200; //ág hossza
+let x: number = 10; //ág osztása
+let t: number = 4; //ágak száma
+let coords: number[] = []; //koordináta tároló
 
-function markCoord(x, y) {
+
+var scale = <HTMLInputElement>document.getElementById("scaleID");
+scale.oninput = function() {
+    h = parseInt (scale.value);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    coords.length = 0;
+    varLength = t * x;
+    star();
+    starCoord();
+  }
+
+  var starCorners = <HTMLInputElement>document.getElementById("starcornerID");
+starCorners.oninput = function() {
+    t = parseInt (starCorners.value);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+/*     console.log ('Starcorner csuszkaval beallitva: ' + x) */
+    coords.length = 0;
+    varLength = t * x;
+    star();
+    starCoord();
+  }
+
+  var divisions = <HTMLInputElement>document.getElementById("divisionsID");
+  divisions.oninput = function() {
+      x = parseInt (divisions.value);
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+/*       console.log ('Starcorner csuszkaval beallitva: ' + x) */
+      coords.length = 0;
+      varLength = t * x;
+      star();
+      starCoord();
+    }
+
+
+
+//ideiglenes koordináta jelölő function
+function markCoord(x: number, y: number) {
   ctx.beginPath();
-  //ctx.moveTo(x, y)
   ctx.arc(x, y, 2, 0, Math.PI * 2);
   ctx.stroke();
 }
 
-function drawLine(x1: number, y1:number, x2: number, y2:number, color) {
+//vonalrajzoló function
+function drawLine(x1: number, y1:number, x2: number, y2:number, color: string) {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
@@ -23,10 +63,7 @@ function drawLine(x1: number, y1:number, x2: number, y2:number, color) {
     ctx.stroke();
 }
 
-let h: number = 100; //ág hossza
-let x: number = 10; //ág osztása
-let t: number = 10; //ágak száma
-let coords: number [][][] = [[[0],[0]]]; //koordináta tároló
+
 
 function star () {
   for (var i = 0; i < t; i++) {
@@ -34,72 +71,61 @@ function star () {
     canvas.width/2,
     canvas.height/2,
     canvas.width/2 + Math.sin((2*Math.PI/t)*i)*h,
-    canvas.height/2 + Math.cos((2*Math.PI/t)*i)*h
+    canvas.height/2 + Math.cos((2*Math.PI/t)*i)*h,
+    'green'
     )
-    markCoord (canvas.width / 2 + Math.sin((2 * Math.PI / t) * i) * h,
-        canvas.height / 2 + Math.cos((2 * Math.PI / t) * i) * h)
+/*     markCoord (canvas.width / 2 + Math.sin((2 * Math.PI / t) * i) * h,
+        canvas.height / 2 + Math.cos((2 * Math.PI / t) * i) * h) */ //Ezt itt visszakapcsolva jelöli az alap csillag végpontjait
 }}
 
-
+let varLength: number = t * x;
 function starCoord () {
+
   for (var i = 0; i < t; i++) {
   for (var j = 0; j < x; j++) {
   
-    markCoord (canvas.width / 2 + Math.sin((2 * Math.PI / t) * i) * h / x * j,
-        canvas.height / 2 + Math.cos((2 * Math.PI / t) * i) * h / x * j);
+/*      markCoord (canvas.width / 2 + Math.sin((2 * Math.PI / t) * i) * h / x * j,
+        canvas.height / 2 + Math.cos((2 * Math.PI / t) * i) * h / x * j); */  //Ezt itt visszakapcsolva jelöli a koordinátákat a vonalakon
         
-        
-}}}
+        coords [varLength] = [j, (canvas.width / 2 + Math.sin((2 * Math.PI / t) * i) * h / x * j), canvas.height / 2 + Math.cos((2 * Math.PI / t) * i) * h / x * j];
+        varLength--
+}}
 
- 
+//utolsó szegmens összekötése
+for (let i = 1; i <= x ; i++) {
+  drawLine (
+    coords [i][1], //x1
+    coords [i][2], //y1
+    coords [x * t - i][1], //x2
+    coords [x * t - i][2], //y2
+    'green')   
+/*     console.log (i) */
+}
 
 
+//összekötő
+let dwn = 0;
+for (let j = 0; j <= (t-1) * x; j+=x){
+  for (let i = 1; i <= x ; i++) {
+      if (dwn === x * (t-1)) {
+      return;
+    }
+    dwn++ 
+    drawLine (
+      coords [i + j][1], //x1
+      coords [i + j][2], //y1
+      coords [2 * x - i + j][1], //x2
+      coords [2 * x - i + j][2], //y2
+      'green')  
+/*       dwn++
+      console.log ('drawline numbers:' + dwn)  */
+  }
+}
+}
 
-
-markCoord (10,10);
 star();
 starCoord();
 
-coords [0][0][0] = 1; //első vonal első x
-coords [0][0][1] = 2; //első vonal első y
-coords [0][1][0] = 3; //első vonal 2. x
-coords [0][1][1] = 4; //első vonal 2. y
-
-/*coords [1][0][0] = 1; //2. vonal első x
-coords [1][0][1] = 2; //2. vonal első y
-coords [1][1][0] = 3; //2. vonal 2. x
-coords [1][1][1] = 4; //2. vonal 2. y*/
-console.log (coords);
-
-
-/*
-//bal alsó kvadráns
-for (let i = 1, j = 20; i <= 20 && j >= 1 ; i++ && j-- ) {
-    drawLine (i * 10 +100, 200, 300, i * 10 +200, 'green')
-    console.log ('i: ' + i)
-    console.log ('j: ' + j)
-}
-
-//jobb alsó kvadráns
-for (let i = 1, j = 20; i <= 20 && j >= 1 ; i++ && j-- ) {
-    drawLine (j * 10 +300, 200, 300, i * 10 +200, 'red')
-    console.log ('i: ' + i)
-    console.log ('j: ' + j)
-}
-
-//jobb felső kvadráns
-for (let i = 1, j = 20; i <= 20 && j >= 1 ; i++ && j-- ) {
-    drawLine (300, i * 10, i * 10 + 300, 200, 'purple')
-    console.log ('i: ' + i)
-    console.log ('j: ' + j)
-}
-//bal felső kvadráns
-for (let i = 1, j = 20; i <= 20 && j >= 1 ; i++ && j-- ) {
-    drawLine (300, i * 10, j * 10 + 100, 200, 'cyan')
-    console.log ('i: ' + i)
-    console.log ('j: ' + j)
-}
+/* 
+coords [varLength+coords[current][0]/t - azaz hanyadik vonal koordinátáit nézzük épp] = [hanyadik koordináta a vonalon, x, y]; //első vonal első x
 */
-
-
-
