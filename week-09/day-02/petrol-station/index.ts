09101ad7c9830415c84e17253138d970a59bc6d3
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-12-04 10:47:04 +0100

    petrol station task added

---
+++ week-09/day-02/petrol-station/index.ts
@@ -0,0 +1,26 @@
+class Station {
+    gasAmount: number;
+    constructor() {
+        this.gasAmount = 1000;
+    }
+    refill(car: Car){
+    station1.gasAmount -= (car.capacity - car.gasAmount)
+    car.gasAmount = car.capacity
+    };
+}
+
+class Car {
+    gasAmount: number;
+    capacity: number;
+    constructor() {
+        this.gasAmount = 0;
+        this.capacity = 100;    
+    }
+
+}
+
+let station1 = new Station;
+let car1 = new Car;
+station1.refill(car1);
+console.log(station1.gasAmount)
+console.log(car1.gasAmount)
