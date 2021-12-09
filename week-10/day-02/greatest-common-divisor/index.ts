92e86d0435c155566fc02acce7bc0672bfc2fc8f
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-12-09 21:31:29 +0100

    greatest common divisor updated with negative exception

--- week-10/day-02/greatest-common-divisor/index.ts
+++ week-10/day-02/greatest-common-divisor/index.ts
@@ -1,11 +1,14 @@
-function gsd(num1:number, num2:number):number {
-    let dividedResult = num1 / num2
-    let dividedRemaining = num1 % num2
+function gsd(num1: number, num2: number): number {
+  let dividedRemaining = num1 % num2;
 
-    if (dividedRemaining === 0) {
-        return num2;
-    }
-    return gsd(num2, dividedRemaining)
+  if (num1 < 0 || num2 < 0) {
+    return -1;
+  }
+
+  if (dividedRemaining === 0) {
+    return num2;
+  }
+  return gsd(num2, dividedRemaining);
 }
 
-console.log(gsd(18, 84))
+console.log(gsd(84, 18));
