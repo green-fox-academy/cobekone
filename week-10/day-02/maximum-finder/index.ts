801f366969493fe56b3caafdaf4becd01b1227b1
Ivanyi Daniel <azivanyidaniel@gmail.com>
2021-12-09 20:51:27 +0100

    maximum-finder task added

---
+++ week-10/day-02/maximum-finder/index.ts
@@ -0,0 +1,20 @@
+function maximumFinder(inputArray:number[]):number[] {
+    
+    if (inputArray.length === 1) {
+        return inputArray;
+    }
+    
+    if (inputArray[0] > inputArray[1]) {
+        inputArray.splice(1, 1)
+       return maximumFinder(inputArray)
+   } else 
+        inputArray.splice(0, 1)
+        return maximumFinder(inputArray)
+}
+
+let randomArray: number[] = [1, 10, 3, 4]
+console.log(maximumFinder(randomArray))
+
+/* console.log(randomArray[0]>randomArray[1])
+randomArray.splice(1,1)
+console.log(randomArray) */
