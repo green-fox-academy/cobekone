8f1d471747edaf683924cfffee0f55da11329d99
Ivanyi Daniel <azivanyidaniel@gmail.com>
2022-02-05 16:41:24 +0100

    svg-1 task added

---
+++ week-14/svg-1/src/index.ts
@@ -0,0 +1,5 @@
+let rectangle = document.querySelector("#very-rectangle");
+
+rectangle.setAttribute("x", "50");
+rectangle.setAttribute("y", "50");
+(rectangle as HTMLElement).style.fill = "tomato";
