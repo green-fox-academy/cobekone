f6cdb859d0cfa9913f9f1f53ba1a62540b8ca27a
Ivanyi Daniel <azivanyidaniel@gmail.com>
2022-02-05 17:10:30 +0100

    greenfox-logo task added

---
+++ week-14/greenfox-logo/src/index.ts
@@ -0,0 +1,4 @@
+let leftEye = document.querySelector("#left-eye");
+(leftEye as HTMLElement).style.fill = "tomato";
+let rightEye = document.querySelector("#right-eye");
+(rightEye as HTMLElement).style.fill = "tomato";
